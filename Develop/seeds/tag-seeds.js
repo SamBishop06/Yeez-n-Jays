d7c00e154dbce45fbc6b6f3f3bbc587d25054509
Angelina Cortes <cortesporcz@gmail.com>
2024-03-13 18:36:13 -0500

    adding product names in entirety

--- Develop/seeds/tag-seeds.js
+++ Develop/seeds/tag-seeds.js
@@ -1,12 +1,12 @@
 const { Tag } = require('../models');
 
 const tagData = [
-    //TODO: Place ids in the '' based off of wireframing data. 
+  //TODO: Place ids in the '' based off of wireframing data.
   {
-    tag_name: '',
+    tag_name: 'Yeezy',
   },
   {
-    tag_name: '',
+    tag_name: 'Jordans',
   },
   {
     tag_name: '',
